0f72d09159798056dec1a578d2932d2fc5f0e6f3
Alex Kuznetsov <lexdev1977@gmail.com~>
2022-03-24 15:31:24 +0600

    Add Set_General, modified: Collection_General, Set_Syntax

--- src/Java_Extend/Java_Collections/Set/Set_Syntax.java
+++ src/Java_Extend/Java_Collections/Set/Set_Syntax.java
@@ -8,22 +8,33 @@ import java.util.TreeSet;
 public class Set_Syntax {
     public static void main(String[] args) {
 
+
+//      создание объектов множества Set
+
+//      HashSet - не дает никаких гарантий относительно последовательности элементов при их итерации
         Set<String> hashSet = new HashSet<>();
+
+//      LinkedHashSet - гарантирует, что порядок элементов во время итерации
+//      совпадает с порядком их вставки в LinkedHashSet.
+//      Повторная вставка элемента, который уже находится в LinkedHashSet, не меняет этот порядок.
         Set<String> linkedHashSet = new LinkedHashSet<>();
+
+//      TreeSet - также гарантирует порядок элементов при повторении,
+//      но он является порядком сортировки элементов.
+//      Другими словами, порядок, в котором элементы должны быть отсортированы,
+//      если вы использовали Collections.sort() для List или массива, содержащего эти элементы.
+//      Этот порядок определяется либо их естественным порядком(если они реализуют Comparable),
+//      либо конкретной реализацией Comparator.
         Set<String> treeSet = new TreeSet<>();
+
+//      добавление элементов в множество метод .add
 
         hashSet.add("Nataly");
         hashSet.add("Kostya");
         hashSet.add("Nikolai");
         hashSet.add("Roman");
         hashSet.add("Iosif");
-
-        for (String name: hashSet){
-            System.out.println(name);
-        }
-        System.out.println();
-
-
+        hashSet.add("Iosif"); // при добавлении элемента с одинаковым именем, он просто игнорируется
 
         linkedHashSet.add("Nataly");
         linkedHashSet.add("Kostya");
@@ -31,24 +42,83 @@ public class Set_Syntax {
         linkedHashSet.add("Roman");
         linkedHashSet.add("Iosif");
 
-        for (String name: linkedHashSet){
-            System.out.println(name);
-        }
-        System.out.println();
-
-
-
-
         treeSet.add("Nataly");
         treeSet.add("Kostya");
         treeSet.add("Nikolai");
         treeSet.add("Roman");
         treeSet.add("Iosif");
 
+//      с помощью foreach  - перебор элементов набора
+
+        for (String name: hashSet){
+            System.out.println(name);
+        }
+        System.out.println();
+
+
+        for (String name: linkedHashSet){
+            System.out.println(name);
+        }
+        System.out.println();
+
+
         for (String name: treeSet){
             System.out.println(name);
         }
         System.out.println();
+
+//      .contains - этот метод проверяет, содержит ли Set данный элемент (объект)
+//       в set этот метод работает очень быстро, так как используется хеширование
+
+        System.out.println(hashSet.contains("Nataly"));
+        System.out.println(hashSet.contains("Alex"));
+        System.out.println();
+
+//      .isEmpty - проверка пустой ли Set или нет
+        System.out.println(hashSet.isEmpty());
+        System.out.println();
+
+//        .toString переопределен
+        System.out.println(hashSet+"\n");
+
+
+
+//      Операции со множествами
+        Set<Integer> set01 = new HashSet<>();
+        Set<Integer> set02 = new HashSet<>();
+
+        set01.add(1);   set02.add(4);
+        set01.add(2);   set02.add(5);
+        set01.add(3);   set02.add(6);
+        set01.add(4);   set02.add(7);
+        set01.add(5);   set02.add(8);
+
+//        в конструктор можно передавать уже готовые множества
+        Set <Integer> union = new HashSet<>(set01);
+        Set <Integer> intersection = new HashSet<>(set01);
+        Set <Integer> difference = new HashSet<>(set01);
+
+//        union - объединение множеств
+
+        System.out.println("Объединение union и set02: " + union +set02);
+        union.addAll(set02);
+        System.out.println("Результат: " + union +"\n");
+
+//        intersection - пересечение множеств
+
+        System.out.println("Пересечение intersection и set02: " + intersection +set02);
+        intersection.retainAll(set02);
+        System.out.println("Результат: " + intersection + "\n");
+
+        //        difference - разность множеств
+
+        System.out.println("Разность difference и set02: " + difference +set02);
+        difference.removeAll(set02);
+        System.out.println("Результат: " + difference + "\n");
+
+
+
+
 
 
 
@@ -56,8 +126,6 @@ public class Set_Syntax {
 
 
     }
-
-
 
 
 
