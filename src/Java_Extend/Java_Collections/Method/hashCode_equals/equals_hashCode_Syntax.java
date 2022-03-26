8ae1d56c47a1e4bb700789b5719ac6391ba4528c
Alex Kuznetsov <lexdev1977@gmail.com~>
2022-03-26 20:31:39 +0600

    modified equals, hashCode

--- src/Java_Extend/Java_Collections/Method/hashCode_equals/equals_hashCode_Syntax.java
+++ src/Java_Extend/Java_Collections/Method/hashCode_equals/equals_hashCode_Syntax.java
@@ -1,6 +1,6 @@
 package Java_Extend.Java_Collections.Method.hashCode_equals;
 
-import java.util.Objects;
+import java.util.*;
 
 public class equals_hashCode_Syntax {
     public static void main(String[] args) {
@@ -33,7 +33,7 @@ public class equals_hashCode_Syntax {
         String m = new String("Hello!");
 
         System.out.println("Сравниваем наш объект строк и созданный Java");
-        System.out.println(n==m);           // выдаст false
+        System.out.println(n == m);           // выдаст false
         System.out.println(n.equals(m));    // выдаст true
         System.out.println();
 
@@ -46,7 +46,7 @@ public class equals_hashCode_Syntax {
 
 
 //      создадим два объекта нашего класса Person с переопределением метод equals и hashCode
-        Person person01 = new Person(1,"Alex");
+        Person person01 = new Person(1, "Alex");
         Person person02 = new Person(1, "Alex");
 
         System.out.println("Сравниваем два объекта класса Person" +
@@ -61,8 +61,7 @@ public class equals_hashCode_Syntax {
 
 
         System.out.println("Проверка строк без учета регистра посредством .equalsIgnoreCase - " +
-                address1.equalsIgnoreCase(address2) +"\n\n");   // выдаст true
-
+                address1.equalsIgnoreCase(address2) + "\n\n");   // выдаст true
 
 
 //                                          ***ПРИМЕРЫ hashCode***
@@ -78,7 +77,7 @@ public class equals_hashCode_Syntax {
 
 
 //        вызов метода hashCode c переопределением и созданием своего алгоритма
-        Person person03 = new Person(1,"Alex");
+        Person person03 = new Person(1, "Alex");
         Person person04 = new Person(2, "Kat");
         System.out.println("вызов hashCode для объекта класса Person (1, \"Alex\")" + person03.hashCode());
         System.out.println("вызов hashCode для объекта класса Person (2, \"Kat\")" + person04.hashCode());
@@ -91,28 +90,118 @@ public class equals_hashCode_Syntax {
                               }
 */
 
+
+        //                             ***ЕЩЕ ПРИМЕНЕНИЕ НА ПРИМЕРАХ LIST И SET***
+
+/*      Смысл такой, есть два контакт с разными ссылками, но с одинаковыми данными самих контактов внутри
+        мы переопределяем метод equals и hashCode таким образом, что сравнение уже проводятс по внутренним полям
+        добавляем мы контакт первый, а ищем или удаляем из колекций уже по второму контакту
+        с переопределенным методом это два одинаковых контакта, с не переопределенным - два разных
+ */
+
+
+        System.out.println("\n\n***ЕЩЕ ПРИМЕНЕНИЕ НА ПРИМЕРАХ LIST И SET***\n");
+
+        ContactNo contactNo01 = new ContactNo(1, "Alex", "0555111222");
+        ContactNo contactNo02 = new ContactNo(1, "Alex", "0555111222");
+
+        Contact contact01 = new Contact(1, "Alex", "0555111222");
+        Contact contact02 = new Contact(1, "Alex", "0555111222");
+
+        System.out.println("хеш кода контактов без переопределения:\n" + contactNo01.hashCode());
+        System.out.println(contactNo02.hashCode());
+        System.out.println("Сравнение по equals - " + contactNo01.equals(contactNo02));
+
+        System.out.println("\nхеш кода контактов c переопределением:\n" + contact01.hashCode());
+        System.out.println(contact02.hashCode());
+        System.out.println("Сравнение по equals - " + contact01.equals(contact02));
+
+
+        System.out.println("\n\nСоздадим ArrayLis на основе наших контактов с и без переопределением");
+
+        System.out.println("\nдобавим первый контакт без переопределения и проверим содержит ли Лист второй контакт," +
+                "\nа также проверим его индекс и удалим его из Листа контактов");
+        List <ContactNo> contactNoList = new ArrayList<>();
+        contactNoList.add(contactNo01);
+        System.out.println("Поиск контакта - " + contactNoList.contains(contactNo02));
+        System.out.println("Индекс контакта - " + contactNoList.indexOf(contactNo02));
+        contactNoList.remove(contactNo02);
+        System.out.println("Размер Листа - " + contactNoList.size());
+
+        System.out.println("\nдобавим первый контакт с переопределением и проверим содержит ли Лист второй контакт," +
+                "\nа также проверим его индекс и удалим его из Листа контактов");
+        List <Contact> contactList = new ArrayList<>();
+        contactList.add(contact01);
+        System.out.println("Поиск контакта - " + contactList.contains(contact02));
+        System.out.println("Индекс контакта - " + contactList.indexOf(contact02));
+        contactList.remove(contact02);
+        System.out.println("Размер Листа - " + contactList.size());
+
+
+        System.out.println("\n\nСоздадим HashSet на основе наших контактов с и без переопределением");
+
+        System.out.println("\nмножество с добавлением контактов без переопределения:");
+        Set <ContactNo> contactNoHashSet= new HashSet<>();
+        contactNoHashSet.add(contactNo01);
+        contactNoHashSet.add(contactNo02);
+        System.out.println(contactNoHashSet);
+        System.out.println("Размер множества - " + contactNoHashSet.size());
+
+        System.out.println("\nмножество с добавлением контактов c переопределением:");
+        Set <Contact> contactHashSet= new HashSet<>();
+        contactHashSet.add(contact01);
+        contactHashSet.add(contact02);
+        System.out.println(contactHashSet);
+        System.out.println("Размер множества - " + contactHashSet.size());
+
+//      Далее ма создаем множество на основе контактов у которых методы переопределенны таким образом,
+//      что в них участвует только поле ID, это позволит перезаписывать данный в контакте, например
+//      если поменялся только телефон
+
+        System.out.println("\nсоздадим объекты с переопределенными методами на основе одного поля ID:");
+        System.out.println("\nтакже создадим множество и добавим в него наш объект");
+
+        ContactID contactID01 = new ContactID(1, "Alex", "0555111222");
+
+        Set <ContactID> contactIDSet = new HashSet<>();
+        contactIDSet.add(contactID01);
+
+        System.out.println("сейчас в нашем множестве такой контакт - " + contactIDSet);
+
+//      Теперь мы хотим обновить телефон у нашего контакта, удаляем старый контакт,
+//      добавляем новый
+//      если это делать без переопределения метода, мы просто добавим еще один контакт
+
+        ContactID contactID02 = new ContactID(1, "Alex", "0555333333");
+        contactIDSet.remove(contactID02);
+        contactIDSet.add(contactID02);
+
+        System.out.println("стал такой контакт - " + contactIDSet);
+
+
     }
 
 
 
-//                  ***ЧАСТЬ СОЗДАНИЯ ДВУХ КЛАССОВ С ПЕРЕОПРЕДЕЛЕНИЕМ МЕТОДОВ И БЕЗ***
+//                  ***ЧАСТЬ СОЗДАНИЯ КЛАССОВ С ПЕРЕОПРЕДЕЛЕНИЕМ МЕТОДОВ И БЕЗ***
 
-//        создамим класс  создадим класс с двумя полями id и name, без переопрделения методов метод equals и hashCode
-    static class Human{
+//            создамим класс  создадим класс с двумя полями id и name, без переопрделения методов метод equals и hashCode
+    static class Human {
         private int id;
         private String name;
 
-    public Human(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+        public Human(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
 
-}
+
+    }
 
 
 //        создадим класс с двумя полями id и name, и переоределим метод equals и hashCode,
 //        чтоб сравнение происходило по этим двум полям
-    static class Person{
+    static class Person {
         private int id;
         private String name;
 
@@ -121,7 +210,7 @@ public class equals_hashCode_Syntax {
             this.name = name;
         }
 
-//            среда автоматически сгенерирует переопределение нажав alt+insert и выбрав раздел "equals() and hashCode()"
+        //            среда автоматически сгенерирует переопределение нажав alt+insert и выбрав раздел "equals() and hashCode()"
 //            далее нужно выбрать поля для сравнения
         @Override
         public boolean equals(Object o) {
@@ -133,8 +222,108 @@ public class equals_hashCode_Syntax {
 
         @Override
         public int hashCode() {
-            return 2 * Objects.hash(id, name) +10000001; // здесь мы сами создали алгоритм хешкода
+            return 2 * Objects.hash(id, name) + 10000001; // здесь мы сами создали алгоритм хешкода
 //                                                          по умолчанию java выдала  - Objects.hash(id, name)
         }
     }
+
+
+//    Еще два класса с переопределенными методами и без
+
+    static class Contact {
+        int id;
+        String name;
+        String phone;
+
+        public Contact(int id, String name, String phone) {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            return "Contact{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", phone='" + phone + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Contact contact = (Contact) o;
+            return id == contact.id && Objects.equals(name, contact.name) && Objects.equals(phone, contact.phone);
+        }
+
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, phone);
+        }
+
+    }
+
+
+    static class ContactNo {
+        int id;
+        String name;
+        String phone;
+
+        public ContactNo(int id, String name, String phone) {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            return "ContactNo{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", phone='" + phone + '\'' +
+                    '}';
+        }
+    }
+
+    static class ContactID {
+        int id;
+        String name;
+        String phone;
+
+        public ContactID(int id, String name, String phone) {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            return "ContactNo{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", phone='" + phone + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ContactID contactID = (ContactID) o;
+            return id == contactID.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+    }
+
+
 }
+
+
+
